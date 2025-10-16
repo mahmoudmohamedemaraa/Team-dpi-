5a05120e4b1996bc7a56d40a002322167f7a56b0
amany2712 <am0932532@gmail.com>
2025-10-17 00:39:00 +0300

    add app theme

---
+++ lib/app_theme.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primaryColor = Color(0xFFDD3F34);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF34383B);
+  static const Color grey = Color(0xFFF5F5F5);
+  static const lightRed = Color(0xFFFAD1CF);
+  static const Color lightGrey = Color(0xFFDEDEDE);
+
+  
+ static ThemeData lightTheme = ThemeData(
+  primaryColor: primaryColor,
+  scaffoldBackgroundColor: white,
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    type: BottomNavigationBarType.fixed,
+    backgroundColor: white,
+    selectedItemColor:primaryColor,
+    unselectedItemColor: lightGrey,
+    showUnselectedLabels: false,
+    showSelectedLabels: false,
+
+  ),
+
+   textTheme: TextTheme(
+    titleMedium: TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      color: black
+    ),
+    titleSmall: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: black
+    )
+  ) ,
+
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: primaryColor,
+      padding: EdgeInsets.only(top: 8),
+      
+    )
+  ),
+
+ );
+
+ // dark theme
+
+  static ThemeData darkTheme = ThemeData(
+   primaryColor: primaryColor,
+  scaffoldBackgroundColor: black,
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    type: BottomNavigationBarType.fixed,
+    backgroundColor: black,
+    selectedItemColor:primaryColor,
+    unselectedItemColor: white,
+    showUnselectedLabels: false,
+    showSelectedLabels: false,
+
+  ),
+
+  textTheme: TextTheme(
+    titleMedium: TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      color: white
+    ),
+    titleSmall: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: white
+    )
+  ) ,
+
+   elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: primaryColor,
+      padding: EdgeInsets.only(top: 8),
+      
+    )
+  ),
+  );
+
+
+
+
+}
