d81ba0b4a093d4f7fc1f92a84e78488b453334dd
Mahmoud Sultan <sultanmahmoud988@gmail.com>
2025-10-26 21:16:18 +0300

    My edits on the project

---
+++ lib/sultan.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Center(child: Text("sultan"))),
+    );
+  }
+}
