15338652222f853b837f7610a1b97b92c41017cd
Mahmoud Sultan <sultanmahmoud988@gmail.com>
2025-10-29 13:29:44 +0300

    Create a security tips page

--- lib/features/auth/presentation/views/widgets/home_widget/home_layout.dart
+++ lib/features/auth/presentation/views/widgets/home_widget/home_layout.dart
@@ -1,6 +1,7 @@
 import 'package:depi_project/app_theme.dart';
 import 'package:depi_project/features/auth/presentation/views/widgets/home_widget/widget_awareness_card.dart';
 import 'package:depi_project/features/auth/presentation/views/widgets/home_widget/widget_quick_options.dart';
+import 'package:depi_project/features/auth/presentation/views/widgets/security_tips_page.dart';
 import 'package:flutter/material.dart';
 
 class HomeLayout extends StatelessWidget {
@@ -12,7 +13,7 @@ class HomeLayout extends StatelessWidget {
       backgroundColor: AppTheme.white,
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(right: 16,left: 16),
+          padding: const EdgeInsets.only(right: 16, left: 16),
           child: ListView(
             children: [
               Row(
@@ -23,63 +24,66 @@ class HomeLayout extends StatelessWidget {
                       CircleAvatar(
                         backgroundColor: AppTheme.grey,
                         radius: 30,
-                        child: Icon(Icons.person,size: 30,color: AppTheme.lightGrey,),
+                        child: Icon(
+                          Icons.person,
+                          size: 30,
+                          color: AppTheme.lightGrey,
+                        ),
                       ),
-                      SizedBox(width: 16,),
+                      SizedBox(width: 16),
                       Column(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("مرحبا بك",
+                          Text(
+                            "مرحبا بك",
                             style: TextStyle(
-                                color: AppTheme.darkGrey,
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold
+                              color: AppTheme.darkGrey,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
                             ),
+                          ),
+                          Text(
+                            "محمد احمد",
+                            style: TextStyle(
+                              color: AppTheme.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
                             ),
-                            Text("محمد احمد",
-                              style: TextStyle(
-                                  color: AppTheme.black,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold
-                              ),
-                              ),
+                          ),
                         ],
                       ),
                     ],
                   ),
-                  Icon(
-                   Icons.settings,
-                   size: 30,
-                   color: AppTheme.darkGrey,
-                  ),
+                  Icon(Icons.settings, size: 30, color: AppTheme.darkGrey),
                 ],
               ),
-              SizedBox(height: 32,),
+              SizedBox(height: 32),
               Container(
                 width: double.infinity,
-                height: MediaQuery.of(context).size.height*0.25,
+                height: MediaQuery.of(context).size.height * 0.25,
                 child: Stack(
                   children: [
                     ClipRRect(
                       borderRadius: BorderRadius.circular(10),
-                      child: Image.asset("assets/images/police.jpg",
-                      width: double.infinity,
-                      fit: BoxFit.cover,
+                      child: Image.asset(
+                        "assets/images/police.jpg",
+                        width: double.infinity,
+                        fit: BoxFit.cover,
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(right: 32,top: 32),
+                      padding: const EdgeInsets.only(right: 32, top: 32),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         spacing: 16,
                         children: [
                           Container(
-                            width: MediaQuery.of(context).size.width*0.12,
-                            height: MediaQuery.of(context).size.height*0.06,
+                            width: MediaQuery.of(context).size.width * 0.12,
+                            height: MediaQuery.of(context).size.height * 0.06,
                             decoration: BoxDecoration(
                               color: AppTheme.darkGrey,
-                              borderRadius: BorderRadius.circular(20)
+                              borderRadius: BorderRadius.circular(20),
                             ),
                             child: Icon(
                               Icons.add_box,
@@ -87,28 +91,31 @@ class HomeLayout extends StatelessWidget {
                               size: 20,
                             ),
                           ),
-                          Text(" تقديم بلاغ جديد",
-                          style: TextStyle(
-                            color: AppTheme.black,
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold
-                          ),
+                          Text(
+                            " تقديم بلاغ جديد",
+                            style: TextStyle(
+                              color: AppTheme.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                           Column(
                             spacing: 8,
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text("يمكنك تقديم بلاغ جديد  ",
-                              style: TextStyle(
-                                color: AppTheme.black,
-                                fontSize: 12,
+                              Text(
+                                "يمكنك تقديم بلاغ جديد  ",
+                                style: TextStyle(
+                                  color: AppTheme.black,
+                                  fontSize: 12,
+                                ),
                               ),
-                              ),
-                              Text("بسهولة وبسرعة",
-                              style: TextStyle(
-                                color: AppTheme.black,
-                                fontSize: 12,
-                              ),
+                              Text(
+                                "بسهولة وبسرعة",
+                                style: TextStyle(
+                                  color: AppTheme.black,
+                                  fontSize: 12,
+                                ),
                               ),
                             ],
                           ),
@@ -117,37 +124,64 @@ class HomeLayout extends StatelessWidget {
                     ),
                   ],
                 ),
-                
-
               ),
-              SizedBox(height: 32,),
-              Text("خيارات سريعة",
-              style: TextStyle(
-                color: AppTheme.black,
-                fontSize: 16,
-                fontWeight: FontWeight.bold
-
+              SizedBox(height: 32),
+              Text(
+                "خيارات سريعة",
+                style: TextStyle(
+                  color: AppTheme.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              ),
-              SizedBox(height: 16,),
+              SizedBox(height: 16),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Expanded(child: WidgetQuickOptions(icon: Icons.receipt_long, text: " بلاغاتي",)),
-                  SizedBox(width: 16,),
-                  Expanded(child: WidgetQuickOptions(icon: Icons.security, text: "نصائح امنيه",)),
+                  Expanded(
+                    child: WidgetQuickOptions(
+                      icon: Icons.receipt_long,
+                      text: " بلاغاتي",
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => SecurityTipsPage(),
+                          ),
+                        );
+                      },
+                      child: WidgetQuickOptions(
+                        icon: Icons.security,
+                        text: "نصائح امنيه",
+                      ),
+                    ),
+                  ),
                 ],
               ),
-              SizedBox(height: 8,),
+              SizedBox(height: 8),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Expanded(child: WidgetQuickOptions(icon: Icons.emoji_events, text: "المكافات",)),
-                  SizedBox(width: 16,),
-                  Expanded(child: WidgetQuickOptions(icon: Icons.phone, text: " ارقام طوارئ",)),
+                  Expanded(
+                    child: WidgetQuickOptions(
+                      icon: Icons.emoji_events,
+                      text: "المكافات",
+                    ),
+                  ),
+                  SizedBox(width: 16),
+                  Expanded(
+                    child: WidgetQuickOptions(
+                      icon: Icons.phone,
+                      text: " ارقام طوارئ",
+                    ),
+                  ),
                 ],
               ),
-              SizedBox(height: 32,),
+              SizedBox(height: 32),
               WidgetAwarenessCard(
                 text1: "توعية الوزارة",
                 text2: " تحذير من الاحتيال السيبراني",
@@ -155,20 +189,14 @@ class HomeLayout extends StatelessWidget {
                 imageUrl: "assets/images/security.jpg",
                 text4: "والاحتيال والتهديد",
               ),
-               SizedBox(height: 16,),
+              SizedBox(height: 16),
               WidgetAwarenessCard(
                 text1: "توعية الوزارة",
                 text2: " التبليغ عن الجرائم الالكترونية",
                 text3: "ابلغ عن الجرائم الالكترونية والحتيال",
                 imageUrl: "assets/images/computer2.jpg",
                 text4: "السيبراني لحماية نفسك",
-              )
-
-              
-
-              
-              
-
+              ),
             ],
           ),
         ),
